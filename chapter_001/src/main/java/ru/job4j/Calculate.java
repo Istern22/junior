9201a57c7e57226a622e07ab59a67285c36f2e3c
Istern22 <alistern22@gmail.com>
2018-01-19 22:35:34 +1000

    1.3. JavaDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,3 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @author Svetlana Ragulina (alistern22@gmail.com)
+* @version 1.0
+*/
  package ru.job4j;
  
  class Calculate {
---
+++ package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+*
+* @author Svetlana Ragulina (alistern22@gmail.com)
+* @version 1.0
+*/
+package ru.job4j;
