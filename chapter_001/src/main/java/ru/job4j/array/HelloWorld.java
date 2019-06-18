835e3384f9ccbd1492d558b3f94d11169cf62abf
Istern22 <alistern22@gmail.com>
2019-06-18 21:56:33 +1000

    Добавлен проект puzzle

---
+++ chapter_001/src/main/java/ru/job4j/array/HelloWorld.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+/*public class HelloWorld {
+    static void changeIt(String value) {
+        value = new String("Hello!");
+    }
+    public static void main(String[] argvc)
+    {
+        String test = new String("Hello World!");
+        changeIt(test);
+        System.out.println("After changing : " +
+                test);
+    }
+}*/
+
+public class HelloWorld {
+    private int iValue;
+    static void changeIt(HelloWorld value) {
+
+        value.iValue = 10;
+    }
+    public static void main(String[] argvc) {
+        HelloWorld hw = new HelloWorld();
+        hw.iValue = 20;
+        changeIt(hw);
+        System.out.println("After changing : "
+                +
+                hw.iValue);
+    }
+}
