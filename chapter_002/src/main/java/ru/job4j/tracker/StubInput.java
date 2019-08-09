3d1b5ffaad8eb6fbb6f0f4265503aa7dc39dcacb
Istern22 <alistern22@gmail.com>
2019-08-09 13:37:35 +1000

    Обеспечить бесперебойную работу приложения Tracker. [#789]

--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -25,7 +25,20 @@ public class StubInput implements Input {
     }
 
     public int ask(String question, List<Integer> range) {
-        return 0;
+
+            int key = Integer.valueOf(this.ask(question));
+            boolean exist = false;
+            for (int value : range) {
+                if (value == key) {
+                    exist = true;
+                    break;
+                }
+            }
+            if (exist) {
+                return key;
+            } else {
+                throw new MenuOutException("Out of menu range.");
+            }
     }
 
     /**
