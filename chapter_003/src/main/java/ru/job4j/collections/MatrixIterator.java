03716bd93bde1b4ca87b9f05866f542e048a866d
Istern22 <alistern22@gmail.com>
2020-02-05 22:10:03 +1000

    MatrixIterator

---
+++ chapter_003/src/main/java/ru/job4j/collections/MatrixIterator.java
@@ -0,0 +1,36 @@
+package ru.job4j.collections;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIterator implements Iterator {
+
+    private final int[][] values;
+    private int row = 0;
+    private int column = 0;
+
+    public MatrixIterator(final int[][] values) {
+        this.values = values;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return  row < values.length && (row != values.length - 1 || column < values[row].length);
+    }
+
+    @Override
+    public Object next() {
+        Object result;
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        result = values[row][column];
+        if (column == values[row].length - 1) {
+            row++;
+            column = 0;
+        } else {
+            column++;
+        }
+        return result;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/collections/MatrixIteratorTest.java
@@ -0,0 +1,89 @@
+package ru.job4j.collections;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class MatrixIteratorTest {
+
+    private Iterator<Integer> it = new MatrixIterator(new int[][] {{1}, {3, 4}, {7}});
+
+    @Test
+    public void testsThatNextMethodDoesNotDependsOnPriorHasNextInvocation() {
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesNotAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(7));
+    }
+
+    @Test
+    public void hasNextNextSequentialInvocation() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(3));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(7));
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test
+    public void whenIterateFive() {
+        int[][] array = {{1}, {3, 4}, {7, 8}};
+        Iterator<Integer> iterator = new MatrixIterator(array);
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(1));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(3));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(4));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(7));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(8));
+        assertThat(iterator.hasNext(), is(false));
+    }
+
+    @Test
+    public void whenIterateThree() {
+        int[][] array = {{1}, {2, 3}};
+        Iterator<Integer> iterator = new MatrixIterator(array);
+        assertThat(iterator.next(), is(1));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(2));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(3));
+        assertThat(iterator.hasNext(), is(false));
+    }
+
+    @Test
+    public void whenArrayLength() {
+        int[][] array = {{1}, {2, 3, 4, 5}, {6, 7}, {8, 9, 10, 11, 12, 13, 14}};
+        assertThat(array.length, is(4));
+        assertThat(array[array.length - 1].length, is(7));
+    }
+
+    @Test
+    public void whenOneItem() {
+        int[][] array = {{1}};
+        Iterator<Integer> iterator = new MatrixIterator(array);
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(1));
+        assertThat(iterator.hasNext(), is(false));
+    }
+}
