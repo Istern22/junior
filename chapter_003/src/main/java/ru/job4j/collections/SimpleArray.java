5cf928bab72b99042e5b7c0c05089e0e22885d99
Istern22 <alistern22@gmail.com>
2020-02-11 18:20:28 +1000

    SimpleArray

---
+++ chapter_003/src/main/java/ru/job4j/collections/SimpleArray.java
@@ -0,0 +1,64 @@
+package ru.job4j.collections;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleArray<T> implements Iterable {
+
+    private Object[] items;
+    private int index = 0;
+
+    public SimpleArray(int size) {
+        this.items = new Object[size];
+    }
+
+    public void add(T model) {
+        this.items[index++] = model;
+    }
+
+    public void set(int position, T model) {
+        if (position < 0 || position > index) {
+            throw new IndexOutOfBoundsException();
+        }
+        this.items[position] = model;
+    }
+
+    public T get(int position) {
+        if (position < 0 || position > index) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (T) this.items[position];
+    }
+
+    public void remove(int position) {
+        if (position < 0 || position > index) {
+            throw new IndexOutOfBoundsException();
+        }
+        System.arraycopy(items, position + 1, items, position, items.length - 1 - position);
+        index--;
+    }
+
+    @Override
+    public Iterator iterator() {
+
+        return new Iterator() {
+
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < index;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T result = (T) items[currentIndex];
+                currentIndex++;
+                return result;
+            }
+        };
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/collections/SimpleArrayTest.java
@@ -0,0 +1,107 @@
+package ru.job4j.collections;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.*;
+
+public class SimpleArrayTest {
+
+
+    @Test
+    public void whenGetItems() {
+        SimpleArray<Integer> intArray = new SimpleArray<>(5);
+        intArray.add(1);
+        intArray.add(null);
+        intArray.add(3);
+        assertThat(intArray.get(0), is(1));
+        assertThat(intArray.get(1), is(nullValue()));
+        assertThat(intArray.get(2), is(3));
+        intArray.set(0, 22);
+        assertThat(intArray.get(0), is(22));
+        intArray.remove(0);
+        assertThat(intArray.get(0), is(nullValue()));
+    }
+
+    @Test
+    public void whenRemoveItems() {
+        SimpleArray<Integer> intArray = new SimpleArray<>(3);
+        intArray.add(1);
+        intArray.add(2);
+        intArray.add(0);
+        assertThat(intArray.get(0), is(1));
+        intArray.remove(0);
+        assertThat(intArray.get(0), is(2));
+        assertThat(intArray.get(1), is(0));
+        assertThat(intArray.get(2), is(0));
+    }
+
+    @Test
+    public void whenIterate() {
+        SimpleArray<Integer> intArray = new SimpleArray<>(3);
+        intArray.add(1);
+        intArray.add(2);
+        var it = intArray.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(false));
+
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void whenNull() {
+        SimpleArray<Integer> intArray = new SimpleArray<>(3);
+        intArray.add(1);
+        intArray.add(2);
+        intArray.remove(3);
+        assertThat(intArray.get(3), is(0));
+    }
+
+    @Test
+    public void whenNullItems() {
+        SimpleArray<Integer> intArray = new SimpleArray<>(3);
+        intArray.add(1);
+        intArray.add(2);
+        assertThat(intArray.get(2), is(nullValue()));
+    }
+
+    @Test
+    public void whenRemove() {
+        SimpleArray<Integer> intArray = new SimpleArray<>(5);
+        intArray.add(1);
+        intArray.add(2);
+        intArray.add(3);
+        intArray.add(4);
+        intArray.add(5);
+        intArray.remove(1);
+        assertThat(intArray.get(1), is(3));
+    }
+
+    @Test
+    public void whenIterateEmptyArray() {
+        SimpleArray<Integer> intArray = new SimpleArray<>(10);
+        assertThat(intArray.iterator().hasNext(), is(false));
+    }
+
+    @Test
+    public void whenIterateArrayWithEmptyElements() {
+        SimpleArray<Integer> intArray = new SimpleArray<>(6);
+        intArray.add(null);
+        intArray.add(1);
+        intArray.add(null);
+        Iterator<Integer> it = intArray.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(nullValue()));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(nullValue()));
+        assertThat(it.hasNext(), is(false));
+    }
+}
