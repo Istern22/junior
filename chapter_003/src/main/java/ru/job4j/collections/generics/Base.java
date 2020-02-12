5f582575b0e5b13c7cba25bf29ad74b235cdd19c
Istern22 <alistern22@gmail.com>
2020-02-12 16:58:06 +1000

    Store<T extends Base>

---
+++ chapter_003/src/main/java/ru/job4j/collections/generics/Base.java
@@ -0,0 +1,32 @@
+package ru.job4j.collections.generics;
+
+public abstract class Base {
+    private final String id;
+
+    protected Base(final String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Base base = (Base) obj;
+
+        return this.id.equals(base.getId());
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/collections/generics/UserStore.java
@@ -0,0 +1,4 @@
+package ru.job4j.collections.generics;
+
+public class UserStore extends AbstractStore<User> {
+}
