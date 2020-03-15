1d0cd33a40e7e1c3cdcdf9cc3e8ed6bea0baf75c
Istern22 <alistern22@gmail.com>
2020-03-15 21:26:17 +1000

    Tree

---
+++ chapter_003/src/main/java/ru/job4j/collections/tree/Tree.java
@@ -0,0 +1,39 @@
+package ru.job4j.collections.tree;
+
+import java.util.*;
+
+class Tree<E> implements SimpleTree<E> {
+    private final Node<E> root;
+
+    Tree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean result = false;
+        var parentNode = findBy(parent);
+        var childNode = findBy(child);
+        if (parentNode.isPresent() && !childNode.isPresent()) {
+            parentNode.get().children.add(new Node<E>(child));
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+}
