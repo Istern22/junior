385d9b5aa0cd5b93f2e601361f3830f910817b61
Istern22 <alistern22@gmail.com>
2020-10-06 07:27:54 +1000

    Крестики-нолики ООД[793#231944]

---
+++ chapter_005/src/main/java/ru/job4j/tictactoe/Desk3х3.java
@@ -0,0 +1,8 @@
+package ru.job4j.tictactoe;
+
+public class Desk3х3 extends Desk {
+
+    public Desk3х3() {
+        super(3);
+    }
+}
---
+++ chapter_005/src/main/java/ru/job4j/tictactoe/Desk5x5.java
@@ -0,0 +1,7 @@
+package ru.job4j.tictactoe;
+
+public class Desk5x5 extends Desk {
+    public Desk5x5() {
+        super(5);
+    }
+}
