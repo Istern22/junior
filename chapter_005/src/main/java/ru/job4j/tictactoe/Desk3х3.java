e9540be16f876d8bfc6dabf0c66ac0079d637fb2
Istern22 <alistern22@gmail.com>
2020-10-06 16:03:13 +1000

    Revert "Крестики-нолики ООД[793#231944]"
    This reverts commit 385d9b5a

--- chapter_005/src/main/java/ru/job4j/tictactoe/Desk3х3.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.tictactoe;
-
-public class Desk3х3 extends Desk {
-
-    public Desk3х3() {
-        super(3);
-    }
-}
--- chapter_005/src/main/java/ru/job4j/tictactoe/Desk5x5.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.tictactoe;
-
-public class Desk5x5 extends Desk {
-    public Desk5x5() {
-        super(5);
-    }
-}
