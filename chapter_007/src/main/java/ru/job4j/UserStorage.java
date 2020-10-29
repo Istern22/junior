5f7a96decabae4ad53771fe50700d2327299d246
Istern22 <alistern22@gmail.com>
2020-10-29 21:12:35 +1000

    Класс хранилища пользователей UserStorage [#388928]

---
+++ chapter_007/src/main/java/ru/job4j/UserStorage.java
@@ -0,0 +1,44 @@
+package ru.job4j;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ThreadSafe
+public class UserStorage {
+
+    private HashMap<Integer, Integer> users = new HashMap<>();
+
+    public synchronized boolean add(User user) {
+        Integer add = users.putIfAbsent(user.getId(), user.getAmount());
+        return add != null;
+    }
+
+    public synchronized boolean update(User user) {
+        Integer update = users.put(user.getId(), user.getAmount());
+        return update != null;
+    }
+
+    public synchronized boolean delete(User user) {
+        Integer remove = users.remove(user.getId());
+        return remove != null;
+    }
+
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        if (users.containsKey(fromId)
+                && users.containsKey(toId)
+                && users.get(fromId) >= amount) {
+            users.put(fromId, users.get(fromId) - amount);
+            users.put(toId, users.get(toId) + amount);
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized HashMap<Integer, Integer> getUsers() {
+        return users;
+    }
+}
